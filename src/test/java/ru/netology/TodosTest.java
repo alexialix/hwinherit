477a385a246a754b346454f469be25e333e1c53a
Alexia <trup.lexa@gmail.com>
2024-11-02 19:32:14 +0500

    Todos updated

--- src/test/java/ru/netology/TodosTest.java
+++ src/test/java/ru/netology/TodosTest.java
@@ -55,4 +55,56 @@ public class TodosTest {
         Task[] actual = todos.search("Бегать");
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldFindMultipleTasksMatchingQuery() {
+        SimpleTask simpleTask = new SimpleTask(1, "Позвонить родителям");
+        String[] subtasks = {"Купить молоко", "Обсуждение новой документации"};
+        Epic epic = new Epic(2, subtasks);
+        Meeting meeting = new Meeting(3, "Обсуждение новой версии", "Проект Альфа", "Среда");
+
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {epic, meeting}; // Ожидаем, что найдутся две задачи
+        Task[] actual = todos.search("новой");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindExactlyOneTaskMatchingQuery() {
+        SimpleTask simpleTask = new SimpleTask(1, "Позвонить родителям");
+        String[] subtasks = {"Купить молоко", "Купить хлеб"};
+        Epic epic = new Epic(2, subtasks);
+        Meeting meeting = new Meeting(3, "Обсуждение новой версии", "Проект Альфа", "Среда");
+
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask}; // Ожидаем, что найдётся ровно одна задача
+        Task[] actual = todos.search("Позвонить");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindNoTasksMatchingQuery() {
+        SimpleTask simpleTask = new SimpleTask(1, "Позвонить родителям");
+        String[] subtasks = {"Купить молоко", "Купить хлеб"};
+        Epic epic = new Epic(2, subtasks);
+        Meeting meeting = new Meeting(3, "Обсуждение новой версии", "Проект Альфа", "Среда");
+
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {}; // Ожидаем, что не найдётся ни одной задачи
+        Task[] actual = todos.search("Танцы");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
+
